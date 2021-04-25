ba3b969e3472c1b105739384b54fb5b25f6998d6
ognjen98 <civcic.ra167.2017@uns.ac.rs>
2021-04-25 20:03:25 +0200

    Updated database with permissions and more authorities

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,5 +1,21 @@
-INSERT INTO USERS (username, password, enabled) VALUES ('admin', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', true);
 
-INSERT INTO AUTHORITY (name) VALUES ('ROLE_ADMIN');
+INSERT INTO USERS (username, password, enabled, role) VALUES ('admin', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', true, 'ADMIN'),
+                                                             ('user', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', true, 'USER');
 
-INSERT INTO USER_AUTHORITY (user_id, authority_id) VALUES (1, 1);
+INSERT INTO AUTHORITY (name) VALUES ('ROLE_ADMIN'),
+                                    ('ROLE_USER');
+
+INSERT INTO PERMISSION (name) VALUES ('CREATE'),
+                                     ('READ'),
+                                     ('UPDATE'),
+                                     ('DELETE');
+
+INSERT INTO USER_AUTHORITY (user_id, authority_id) VALUES (1, 1),
+                                                          (2,2);
+
+INSERT INTO AUTHORITY_PERMISSION (authority_id, permission_id) VALUES (1, 1),
+                                                                      (1,2),
+                                                                      (1,3),
+                                                                      (1,4),
+                                                                      (2,2),
+                                                                      (2,3);
