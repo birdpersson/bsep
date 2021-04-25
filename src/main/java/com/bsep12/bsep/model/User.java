31f616c0a33decf4108702902526b9d5c1f518dc
ognjen98 <civcic.ra167.2017@uns.ac.rs>
2021-04-25 20:05:48 +0200

    Added granted authority for permissions

--- src/main/java/com/bsep12/bsep/model/User.java
+++ src/main/java/com/bsep12/bsep/model/User.java
@@ -2,9 +2,11 @@ package com.bsep12.bsep.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -26,11 +28,14 @@ public class User implements UserDetails {
 	@Column(nullable = false)
 	private boolean enabled;
 
+	@Column
+	private String role;
+
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "user_authority",
 			joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
 			inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
-	private List<Authority> authorities;
+	private List<Authority> roles;
 
 	public Long getId() {
 		return id;
@@ -47,6 +52,20 @@ public class User implements UserDetails {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+		getRoles().forEach(role -> {
+			grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+			role.getPermissions().forEach(permission -> {
+				grantedAuthorities.add(new SimpleGrantedAuthority(permission.getName()));
+			});
+
+		});
+		return grantedAuthorities;
 	}
 
 	@Override
@@ -67,13 +86,18 @@ public class User implements UserDetails {
 		this.enabled = enabled;
 	}
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return this.authorities;
+	public String getRole() {
+		return role;
 	}
 
-	public void setAuthorities(List<Authority> authorities) {
-		this.authorities = authorities;
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public List<Authority> getRoles() { return this.roles;}
+
+	public void setRoles(List<Authority> authorities) {
+		this.roles = authorities;
 	}
 
 	@JsonIgnore
